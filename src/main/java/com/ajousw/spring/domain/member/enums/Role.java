bb5e39b4e690628edf696cff47c8babc5edc59d1
bandall <jsm5315@naver.com>
2024-01-09 00:16:49 +0900

    refactor: db 변경
    MySQL에서 PostgreSQL로 DB 변경

--- src/main/java/com/ajousw/spring/domain/member/enums/Role.java
+++ src/main/java/com/ajousw/spring/domain/member/enums/Role.java
@@ -2,6 +2,7 @@ package com.ajousw.spring.domain.member.enums;
 
 public enum Role {
     ROLE_USER("ROLE_USER"),
+    ROLE_EMERGENCY_VEHICLE("ROLE_EMERGENCY_VEHICLE"),
     ROLE_ANONYMOUS("ROLE_ANONYMOUS"),
     ROLE_ADMIN("ROLE_ADMIN");
 
