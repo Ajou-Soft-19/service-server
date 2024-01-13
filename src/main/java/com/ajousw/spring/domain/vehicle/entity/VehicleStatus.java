453bfc17809adc1323b68606f79aed356cac1e1e
bandall <jsm5315@naver.com>
2024-01-14 02:04:58 +0900

    refactor: VehicleStatus 수정
    VehicleStatus 엔티티 수정

--- src/main/java/com/ajousw/spring/domain/vehicle/entity/VehicleStatus.java
+++ src/main/java/com/ajousw/spring/domain/vehicle/entity/VehicleStatus.java
@@ -3,13 +3,10 @@ package com.ajousw.spring.domain.vehicle.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,8 +19,7 @@ import org.locationtech.jts.geom.Point;
 public class VehicleStatus {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID vehicleStatusId;
+    private String vehicleStatusId;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vehicleId")
@@ -34,28 +30,29 @@ public class VehicleStatus {
     @Column(columnDefinition = "geometry(Point, 4326)")
     private Point coordinate;
 
-    private int kmPerHour;
+    private double meterPerSec;
 
-    private int direction;
+    private double direction;
 
     private LocalDateTime lastUpdateTime;
 
     @Builder
-    public VehicleStatus(Vehicle vehicle, boolean usingNavi, Point coordinate, int kmPerHour,
-                         int direction, LocalDateTime lastUpdateTime) {
+    public VehicleStatus(String vehicleStatusId, Vehicle vehicle, boolean usingNavi, Point coordinate,
+                         double meterPerSec, double direction, LocalDateTime lastUpdateTime) {
+        this.vehicleStatusId = vehicleStatusId;
         this.vehicle = vehicle;
         this.usingNavi = usingNavi;
         this.coordinate = coordinate;
-        this.kmPerHour = kmPerHour;
+        this.meterPerSec = meterPerSec;
         this.direction = direction;
         this.lastUpdateTime = lastUpdateTime;
     }
 
-    public void modifyStatus(boolean usingNavi, Point coordinate, int kmPerHour,
-                             int direction, LocalDateTime lastUpdateTime) {
+    public void modifyStatus(boolean usingNavi, Point coordinate, double meterPerSec,
+                             double direction, LocalDateTime lastUpdateTime) {
         this.usingNavi = usingNavi;
         this.coordinate = coordinate;
-        this.kmPerHour = kmPerHour;
+        this.meterPerSec = meterPerSec;
         this.direction = direction;
         this.lastUpdateTime = lastUpdateTime;
     }
