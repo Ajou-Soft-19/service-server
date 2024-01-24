da49b2e6cc933b73e9a0929c187fac971f307f1d
bandall <jsm5315@naver.com>
2024-01-25 01:22:09 +0900

    feat: 응급 상황 관리 테이블 추가
    1. EmergencyEvent 엔티티 추가
    응급 상황을 저장하는 테이블 역할
    2. WarnRecord 엔티티 추가
    어떤 체크포인트에서, 어떤 차량이 경고 알림을 받았는지 저장
    3. EmergencyEvent 서비스 및 컨트롤러 추가

---
+++ src/main/java/com/ajousw/spring/web/controller/dto/emergency/EmergencyEventEndDto.java
@@ -0,0 +1,15 @@
+package com.ajousw.spring.web.controller.dto.emergency;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmergencyEventEndDto {
+
+    private Long emergencyEventId;
+
+}
